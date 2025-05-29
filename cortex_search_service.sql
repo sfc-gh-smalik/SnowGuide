b0338928e82d20242bf7665fb39b8aeb8c3c5c2d
Sunny Malik <72890336+sfc-gh-smalik@users.noreply.github.com>
2025-05-28 18:52:22 -0700

    Update cortex_search_service.sql

--- cortex_search_service.sql
+++ cortex_search_service.sql
@@ -2,23 +2,23 @@
 
 -- Create a Cortex Search Service once the URL are scraped and content is pulled successfully
 CREATE OR REPLACE CORTEX SEARCH SERVICE <fully_qualified_service_name>
-  ON content
+  ON chunked_data
   WAREHOUSE = <wh_name>
   TARGET_LAG = '<lag>'
   EMBEDDING_MODEL = 'snowflake-arctic-embed-l-v2.0'
   INITIALIZE = ON_CREATE
   AS 
-    SELECT * FROM <your_fully_qualified_content_table>;
-
+    SELECT u.*, index, value::string as chunked_data FROM <your_fully_qualified_content_table> u,
+    LATERAL FLATTEN(input => SNOWFLAKE.CORTEX.SPLIT_TEXT_RECURSIVE_CHARACTER (cleaned_text,'markdown',2000,300);
 
 
 -- Cortex Search Preview query
 SELECT
   SNOWFLAKE.CORTEX.SEARCH_PREVIEW (
-      'TEMP.SMALIK.DOC_SEARCHER',
+      '<fully_qualified_service_name>',
       '{
           "query": "what connectors are available in openflow",
-          "columns": ["CONTENT", "PARENT_URL"],
+          "columns": ["CHUNKED_DATA", "URL"],
           "limit": 20
       }'
   );
