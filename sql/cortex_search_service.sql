b94261612c6d3b6f5a3a2132b39000fe7f0e08eb
James Barnett <james.barnett@snowflake.com>
2025-06-17 10:16:46 -0700

    Formatting sql

--- sql/cortex_search_service.sql
+++ sql/cortex_search_service.sql
@@ -1,16 +1,16 @@
-
-
 -- Create a Cortex Search Service once the URL are scraped and content is pulled successfully
-CREATE OR REPLACE CORTEX SEARCH SERVICE <fully_qualified_service_name>
+CREATE CORTEX SEARCH SERVICE IF NOT EXISTS <fully_qualified_service_name>
   ON chunked_data
   WAREHOUSE = <wh_name>
-  TARGET_LAG = '<lag>'
+  TARGET_LAG = '<days> days'
   EMBEDDING_MODEL = 'snowflake-arctic-embed-l-v2.0'
-  INITIALIZE = ON_CREATE
+  INITIALIZE = ON_SCHEDULE
   AS 
     SELECT u.*, index, value::string as chunked_data FROM <your_fully_qualified_content_table> u,
     LATERAL FLATTEN(input => SNOWFLAKE.CORTEX.SPLIT_TEXT_RECURSIVE_CHARACTER (cleaned_text,'markdown',2000,300));
 
+ALTER CORTEX SEARCH SERVICE <fully_qualified_service_name> REFRESH;
+ALTER CORTEX SEARCH SERVICE <fully_qualified_service_name> RESUME;
 
 -- Cortex Search Preview query
 SELECT
